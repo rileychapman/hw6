a56f1fdaf6fc3fa1ab31cd427fe42af0af6c1948
Paul Titchener <paul.titchener@students.olin.edu>
2014-03-07 12:33:49 -0500

    Adding sprites that disappear on contact with character

--- jumpguy.py
+++ jumpguy.py
@@ -36,9 +36,23 @@ class JumpGuyModel:
         print 'creating an object'
        
         self.guy = Guy((255,255,255),20,100,200,450,self.size)
-        self.allsprites = pygame.sprite.RenderPlain((self.guy))
+        self.coins = []
+
+        for y in range(0,self.size[1],100):
+            coin = Coin(self,20,y)
+            self.coins.append(coin)
+
+
+
+        self.allsprites = pygame.sprite.Group((self.guy))
+        for coin in self.coins:
+                pygame.sprite.Group.add(self.allsprites,coin)
 
     def update(self):
+        for coin in self.coins:
+            if coin.disappear:
+                self.allsprites.remove(coin)
+                print 'a'
         self.allsprites.update()
 
 
@@ -47,6 +61,26 @@ class JumpGuyModel:
 
 
         
+class Coin(pygame.sprite.Sprite):
+    def __init__(self,model,x,y):
+        self.model = model
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image, self.rect = load_image('finn1.png', -1) #load an image
+        self.x = x 
+        self.y = y
+        self.width = self.rect.width
+        self.height = self.rect.height
+        self.disappear = False 
+        self.rect.topleft = (self.x,self.y)
+    def update(self):
+        #if abs(self.model.guy.x -self.x) < (self.rect.width - self.model.guy.width)/2 and abs(self.model.guy.y -self.y) < (self.rect.height- self.model.guy.height)/2:
+        
+        if abs(self.model.guy.y -self.y) < 10:
+            
+            self.disappear = True
+            print self.disappear
+
 
 
 
@@ -108,20 +142,44 @@ class Guy(pygame.sprite.Sprite):
 
         self.vx = self.vx_inter
 
-        if self.x < -1:
-            self.x = 0
-        elif self.x > self.window_size[0]-self.width:
-            self.x = self.window_size[0]-self.width
-        else:
+        #making code to keep the sprite in the left and right of the window
+
+        LEFT = self.x <= 0
+        RIGHT = self.x >= self.window_size[0]-self.width
+        IN_horoz = not LEFT and not RIGHT
+
+        if LEFT:
+            if self.vx<0:
+                self.x = 0
+            else:
+                self.x += self.vx
+        elif RIGHT:
+            if self.vx >0:
+                self.x = self.window_size[0]-self.width
+            else:
+                self.x += self.vx
+        elif IN_horoz:
             self.x += self.vx
 
-        if self.y < self.window_size[1]-self.height:
-            self.y += self.vy            
-        else:
+        #making code to determine if the sprite is in the window or not
+        UP = self.y <=0
+        DOWN = self.y>= self.window_size[1]-self.height
+        IN = not UP and not DOWN
+
+        if IN:
+            self.y += self.vy
+        elif DOWN:
             if self.vy > 0:
                 self.y = self.window_size[1]-self.height
             else: 
                 self.y +=self.vy
+        elif UP:
+            if self.vy<0:
+                self.y = 0
+            if self.vy>0:
+                self.y += self.vy
+
+
 
         self.rect.topleft = (self.x,self.y)
 
